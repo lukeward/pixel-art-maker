59a9c4239c61811190d2ccc0110b5532adf2d2d1
galvanize6 <lward@us.ci.org>
2019-05-07 16:06:09 -0600

    Perfected color palette.

--- README.md
+++ README.md
@@ -16,8 +16,12 @@ Here's a development workflow that we recommend you use.
 1. Fork and clone this repository.
 1. Create a small, 2x2 grid canvas made up of white, square `div` tags with a border.
 1. Add an event listener to each `div` so when clicked the background turns red.
+
+
 1. Create a small palette of two colors (e.g. red and blue) below the canvas using more `div` tags.
 1. Add an event listener to these `div` tags so when clicked the brush color is saved.
+
+
 1. Expand the dimensions of the pixel canvas.
 1. Expand the palette with more colors. (i.e. red, orange, yellow, green, blue, purple, brown, gray, black, white, etc.)
 1. Expand the palette with a brush color indicator.
---
+++ pixel.js
@@ -0,0 +1,74 @@
+const containerTag = document.querySelector('.container')
+
+// Added the outer pad to the DOM.
+const pad = document.createElement('div')
+pad.className = 'pad'
+containerTag.appendChild(pad)
+
+
+
+const getDiv = (name) => {
+    let result = document.createElement('div')
+    result.className = name
+
+    return result
+}
+
+const boxSize = 20
+const borderSize = 1
+const colorBoxSize = 20
+const colorBorderSize = 1
+const rowCount = 25
+const colCount = 25
+
+pad.style.width = boxSize * colCount
+pad.style.height = boxSize * rowCount
+
+let currentColor = 'white'
+
+for (let col=0; col<colCount; col+=1) {
+    const rowDiv = getDiv('row-div')
+
+    for(let row=0; row<rowCount; row+=1) {
+        const box = document.createElement('div')
+        box.className = 'box'
+        box.style.width = boxSize - 2 * borderSize
+        box.style.height = boxSize - 2 * borderSize
+
+        box.addEventListener('click', (evt)=>{
+            box.style.backgroundColor = currentColor
+            box.style.borderColor = currentColor
+        })
+
+        rowDiv.appendChild(box)
+    }
+    
+    // Adding row of boxes (in theory) to the pad.
+    pad.appendChild(rowDiv)
+}
+
+
+const colors = ['red', 'orange', 'yellow', 'green', 'blue', 'purple', 'brown', 'gray', 'black', 'white']
+
+for (let col=0; col<colors.length; col+=1) {
+    const box = document.createElement('div')
+    box.className = 'color'
+    box.id = colors[col]
+    box.style.backgroundColor = colors[col]
+    box.style.width = colorBoxSize - 2 * colorBorderSize
+    box.style.height = colorBoxSize - 2 * colorBorderSize
+
+    box.addEventListener('click', (evt)=>{
+        previousBox = document.querySelector(`#${currentColor}`)
+        previousBox.style.border = '1px solid black'
+
+        currentColor = evt.srcElement.style.backgroundColor
+        box.style.border = '3px solid black'
+    })
+
+    const colorDiv = getDiv('color-div')
+    colorDiv.appendChild(box)
+
+    containerTag.appendChild(colorDiv)
+}
+
